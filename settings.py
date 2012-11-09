de49bd9c2fd6b8dd888d575ffeb647971dadc428
Ubuntu <ubuntu@awsubuntu1.localdomain>
2012-11-09 03:33:41 +0000

    got media serving working when in debug mode

--- settings.py
+++ settings.py
@@ -52,13 +52,15 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
-
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media/')
+if DEBUG:
+    print MEDIA_ROOT
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+# MEDIA_URL = '/media/'
+MEDIA_URL = '/http://ec2-23-22-28-115.compute-1.amazonaws.com:8000/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -151,3 +153,4 @@ LOGGING = {
         },
     }
 }
+
