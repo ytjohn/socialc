2512371fb447e64a95bb6904a3630d9dfad70727
Ubuntu <ubuntu@awsubuntu1.localdomain>
2012-11-09 04:12:32 +0000

    helloworld view returns random greeting from db

--- urls.py
+++ urls.py
@@ -2,19 +2,19 @@ from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'socialc.views.home', name='home'),
     # url(r'^socialc/', include('socialc.foo.urls')),
-
+    url(r'^$', 'helloworld.views.index', name='default'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
      
 )
